5c0e41d6b06e1a10c198f4ae14e3fd817dec6f1c
gyub <35800649+gaoyubo21@users.noreply.github.com>
2018-11-15 16:44:47 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-
-
 sh_ver="2.0.38"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
